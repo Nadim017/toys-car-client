6987967af2ea668409900f1845d4926443ddd14a
nadim <mdnadimmahamud704@gmail.com>
2023-05-18 08:31:20 +0600

    footer done

--- src/Component/Error/Error.jsx
+++ src/Component/Error/Error.jsx
@@ -5,7 +5,7 @@ const Error = () => {
   return (
     <div className="flex justify-center items-start mt-5 mb-5 ">
       <div>
-        <h3 className="">Page Not Found !!!!!!!!!! 😥</h3>
+        <h3 className="text-center">Page Not Found !!!!!!!!!! 😥</h3>
         <img src="/error.jpg" className="h-50%" alt="" />
         <p className=" mt-3 mb-3">Error : 444000000044444!</p>
         <Link to="/">
